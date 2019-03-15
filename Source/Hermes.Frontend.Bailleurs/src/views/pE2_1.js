f79f06ae769d246198172d3acf92aadfad2d967f
zouaoui <sami.zouaoui-ext@actionlogement.fr>
2019-03-15 16:21:23 +0100

    Gestion des demandes...

--- Source/Hermes.Frontend.Bailleurs/src/views/pE2_1.js
+++ Source/Hermes.Frontend.Bailleurs/src/views/pE2_1.js
@@ -1,5 +1,6 @@
 import { ConsulterDemandes } from "../models/consulter-demandes";
 import { ValiderDemande } from "../models/valider-demande";
+import { GererPriorites } from "../models/gerer-priorites";
 
 export class PageE2_1 {
 
@@ -10,6 +11,7 @@ export class PageE2_1 {
     this.filtres.push({title: 'Veuillez sélectionner un type de filtre', obj: null});
     this.filtres.push({title: 'Consulter des demandes', obj: new ConsulterDemandes()});
     this.filtres.push({title: 'Valider une demande', obj: new ValiderDemande()});
+    this.filtres.push({title: 'Gérer mes priorités', obj: new GererPriorites()});
   }
 
 }
