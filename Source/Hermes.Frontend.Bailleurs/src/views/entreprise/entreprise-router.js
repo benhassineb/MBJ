fa9e40541b35d8b01a30c3d54f2fe86b1fc731c0
zouaoui <sami.zouaoui-ext@actionlogement.fr>
2019-03-15 16:38:39 +0100

    Gestion des demandes

--- Source/Hermes.Frontend.Bailleurs/src/views/entreprise/entreprise-router.js
+++ Source/Hermes.Frontend.Bailleurs/src/views/entreprise/entreprise-router.js
@@ -3,7 +3,7 @@ export class EntrepriseRouter {
   configureRouter(config, router) {
     config.map([
       { route: ['', 'accueil'], name: 'accueil', moduleId: './accueil-entreprise', nav: true, title: 'Accueil' },
-      { route: 'pE2_1', name: 'pE2_1', moduleId: './pE2_1', nav: true, title: 'pE2_1' },
+      { route: 'gestion-demandes', name: 'gestion-demandes', moduleId: './gestion-demandes', nav: true, title: 'Gestion des demandes' },
 
       { route: 'Entreprise', name: 'Entreprise', moduleId: 'entreprise-router', nav: true, title: 'entreprise Router' }
     ]);
--- Source/Hermes.Frontend.Bailleurs/src/views/entreprise/gestion-demandes.js
+++ Source/Hermes.Frontend.Bailleurs/src/views/entreprise/gestion-demandes.js
@@ -1,7 +1,8 @@
 import { ConsulterDemandes } from '../../models/consulter-demandes';
 import { ValiderDemande } from '../../models/valider-demande';
+import { GererPriorites } from '../../models/gerer-priorites';
 
-export class PageE2_1 {
+export class GestionDemandes {
 
   filtres = [];
   selectedFilter;
@@ -10,6 +11,7 @@ export class PageE2_1 {
     this.filtres.push({ title: 'Veuillez sélectionner un type de filtre', obj: null });
     this.filtres.push({ title: 'Consulter des demandes', obj: new ConsulterDemandes() });
     this.filtres.push({ title: 'Valider une demande', obj: new ValiderDemande() });
+    this.filtres.push({title: 'Gérer mes priorités', obj: new GererPriorites()});
   }
 
 }
