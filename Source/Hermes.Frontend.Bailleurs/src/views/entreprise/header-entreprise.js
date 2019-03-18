e1920d7e8eaf8cc50852e9811f8bf3963cda6580
Florian MATHIEU <florian.mathieu@actionlogement.fr>
2019-03-18 17:11:20 +0100

    modif

--- Source/Hermes.Frontend.Bailleurs/src/views/entreprise/header-entreprise.js
+++ Source/Hermes.Frontend.Bailleurs/src/views/entreprise/header-entreprise.js
@@ -1,16 +1,18 @@
 import { inject } from 'aurelia-framework';
 import { SampleService } from 'services/sample-service';
 import {customElement, bindable, bindingMode } from 'aurelia-framework';
+import {Router} from 'aurelia-router';
 
-@inject(SampleService)
+@inject(SampleService, Router)
 @customElement('header-entreprise')
 export class headerentreprise {
 
     listFiltreEntreprise;
     listFiltreEntrepriseSelected = [];
     
-    constructor(service) {
+    constructor(service, parentRouter) {
       this._service = service;
+      this._parentRouter = parentRouter;
       this._service.getFiltreEntreprise()
         .then(result => {this.listFiltreEntreprise = result; this.listFiltreEntrepriseSelected.push(this.listFiltreEntreprise[1]);  });
     }
