668d951f4115a11c45b3e8a3800ccaa4037f0aa6
Honore MAGABABOU <honore.magababou@actionlogement.fr>
2019-03-18 09:58:35 +0100

    ajout html détails demande avec CERFA et sans CERFA

--- Source/Hermes.Frontend.Bailleurs/src/views/entreprise/entreprise-router.js
+++ Source/Hermes.Frontend.Bailleurs/src/views/entreprise/entreprise-router.js
@@ -4,8 +4,10 @@ export class EntrepriseRouter {
     config.map([
       { route: ['', 'accueil'], name: 'accueil', moduleId: './accueil-entreprise', nav: true, title: 'Accueil' },
       { route: 'pE2_1', name: 'pE2_1', moduleId: './pE2_1', nav: true, title: 'pE2_1' },
-
+      { route: 'details-demande-sans-CERFA',  name: 'details-demande-sans-CERFA', moduleId: './details-demande-sans-CERFA', nav: true, title: 'Details demande sans CERFA'  },
+      { route: 'details-demande-avec-CERFA',  name: 'details-demande-avec-CERFA', moduleId: './details-demande-avec-CERFA', nav: true, title: 'Details demande avec CERFA' },
       { route: 'Entreprise', name: 'Entreprise', moduleId: 'entreprise-router', nav: true, title: 'entreprise Router' }
+
     ]);
 
     this.router = router;
