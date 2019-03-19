32855deef9db3d46ecc57a5f6a44a2374d31df3e
Florian MATHIEU <florian.mathieu@actionlogement.fr>
2019-03-19 14:56:12 +0100

    Modif

--- Source/Hermes.Frontend.Bailleurs/src/views/entreprise/header-entreprise.js
+++ Source/Hermes.Frontend.Bailleurs/src/views/entreprise/header-entreprise.js
@@ -2,6 +2,7 @@ import { inject } from 'aurelia-framework';
 import { SampleService } from 'services/sample-service';
 import {customElement, bindable, bindingMode } from 'aurelia-framework';
 import {Router} from 'aurelia-router';
+import {AutoCompleteController} from 'aurelia-autocomplete';
 
 @inject(SampleService, Router)
 @customElement('header-entreprise')
@@ -11,7 +12,6 @@ export class headerentreprise {
     listFiltreEntreprise;
     listFiltreDepartementSelected = [];
     filtreEntrepriseSelected;
-    
     constructor(service, parentRouter) {
       this._service = service;
       this._parentRouter = parentRouter;
@@ -19,17 +19,9 @@ export class headerentreprise {
         .then(result => { this.listFiltreEntreprise = result; this.filtreEntrepriseSelected = result[1];});
       this._service.getFiltreDepartement()
         .then(result => {this.listDepartement = result; this.listFiltreDepartementSelected.push(result[1]); this.listFiltreDepartementSelected.push(result[3]);});
-    }
+      }
 
     removeFiltreEntreprise(filtre) {
-      this.listFiltreDepartementSelected = this.listFiltreDepartementSelected.filter(item => item.code !== filtre.code);
-    }
-    
-    addFiltreEntreprise(filtre) {
-      this.listFiltreDepartementSelected.push(filtre);
-    }
-
-    searchDepartement(value){
-
+      this.listFiltreDepartementSelected = this.listFiltreDepartementSelected.filter(item => item.label !== filtre.label);
     }
 }
