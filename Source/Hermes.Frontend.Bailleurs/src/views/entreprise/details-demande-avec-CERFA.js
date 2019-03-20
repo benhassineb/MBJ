31bbdd2d7f1df5c33df9debebc0ecdb127af85bc
Honore MAGABABOU <honore.magababou@actionlogement.fr>
2019-03-20 14:18:02 +0100

    complétion du json et de la demande

--- Source/Hermes.Frontend.Bailleurs/src/views/entreprise/details-demande-avec-CERFA.js
+++ Source/Hermes.Frontend.Bailleurs/src/views/entreprise/details-demande-avec-CERFA.js
@@ -9,11 +9,16 @@ export class DetailsDemandeAvecCERFA {
   constructor(service) {
     this._service = service;
     this.demande;
+    this.isDisabled = true;
   }
 
   activate() {
     return this._service.getDetailsDemandeAvecCERFA()
       .then(result => this.demande = result);
+  }
+
+  fieldsetDisabled() {  
+    this.isDisabled = false;
   }
 
 }
